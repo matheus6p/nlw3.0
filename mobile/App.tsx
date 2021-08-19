c6c03e0af1ff15f60c42bf0852f274547a7b41ea
Matheus Paes <mhpaes97@gmail.com>
2021-08-19 20:11:51 -0300

    Build

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import { Nunito_600SemiBold,Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+import {useFonts} from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  })
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Routes />
+  );
+}
+
+
---
+++ web/build/precache-manifest.8404e77d4180d24ea4ab46d8caf1d6b1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af245f43cbb013dcb49cad9c220e4571",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ba080831777e4ca5c73",
+    "url": "/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "91189b689626aa2c4e2f",
+    "url": "/static/css/main.f9e453fa.chunk.css"
+  },
+  {
+    "revision": "4ba080831777e4ca5c73",
+    "url": "/static/js/2.3562af9c.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/static/js/2.3562af9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91189b689626aa2c4e2f",
+    "url": "/static/js/main.dabcbaa1.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/static/media/landing.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/static/media/logo.1653c647.svg"
+  },
+  {
+    "revision": "658b83029253fe8e316b8f364db7b424",
+    "url": "/static/media/map-marker.658b8302.svg"
+  }
+]);
